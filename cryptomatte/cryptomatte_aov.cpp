49d4d87a15f807de540a2f593b4b2ccd7bb33cda
Jonah Friedman <jfriedman@psyop.tv>
2017-05-30 12:58:03 -0400

    Applied aov_type patch

--- cryptomatte/cryptomatte_aov.cpp
+++ cryptomatte/cryptomatte_aov.cpp
@@ -39,6 +39,10 @@ node_parameters
    AiParameterStr("user_src_2", "");
    AiParameterStr("user_aov_3", "");
    AiParameterStr("user_src_3", "");
+
+   AiMetaDataSetInt(nentry, "aov_crypto_asset", "aov.type", AI_TYPE_RGBA);
+   AiMetaDataSetInt(nentry, "aov_crypto_object", "aov.type", AI_TYPE_RGBA);
+   AiMetaDataSetInt(nentry, "aov_crypto_material", "aov.type", AI_TYPE_RGBA);
 }
 
 node_initialize
