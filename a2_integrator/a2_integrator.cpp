fca405d95f9b418c4cab7294c9b3ef8a843f0593
Anders Langlands <anderslanglands@gmail.com>
2017-08-21 21:30:21 +1200

    split lut outptu functions to avoid dependencies on oiio

--- a2_integrator/a2_integrator.cpp
+++ a2_integrator/a2_integrator.cpp
@@ -1,4 +1,4 @@
-#include "a2_uber/lut.hpp"
+#include "a2_uber/lut_output.hpp"
 #include "common/util.hpp"
 #include <ai.h>
 #include <cstdlib>
@@ -24,56 +24,6 @@ shader_evaluate {
     AtRGB display = AI_RGB_RED;
     if (sg->x == 0 && sg->y == 0 && getenv("A2_INTEGRATE") != nullptr) {
         auto sg_copy = *sg;
-
-        /*
-        int lut_resolution_i = 32;
-        int lut_resolution_j = 32;
-        int lut_size = lut_resolution_i * lut_resolution_j;
-        auto lut = std::vector<float>(lut_resolution_i * lut_resolution_j);
-
-        for (int j = 0; j < lut_resolution_j; ++j) {
-            // invert mu as we don't want to capture mu=0
-            auto mu = 1.0f - float(j) / float(lut_resolution_j);
-            auto sin_theta = cosf(asinf(mu));
-            auto omega_o = AtVector(sin_theta, mu, 0.0f);
-            sg_copy.Rd = -omega_o;
-            for (int i = 0; i < lut_resolution_i; ++i) {
-                float r = float(i) / float(lut_resolution_i - 1);
-                auto idx = j * lut_resolution_i + i;
-                auto bsdf_mf =
-                    AiMicrofacetBSDF(&sg_copy, AI_RGB_WHITE, AI_MICROFACET_GGX,
-                                     sg_copy.Nf, &U, 0.0f, r, r);
-                auto mtd = AiBSDFGetMethods(bsdf_mf);
-                mtd->Init(&sg_copy, bsdf_mf);
-                float result = 0.0f;
-                int samples = 100;
-                int ns = 0;
-                for (int x = 0; x < samples; ++x) {
-                    for (int y = 0; y < samples; ++y) {
-                        float xx = float(x) / float(samples);
-                        float yy = float(y) / float(samples);
-                        AtVectorDv out_wi;
-                        int out_lobe_index;
-                        AtBSDFLobeSample lobe_sample;
-                        auto lobe_mask = mtd->Sample(
-                            bsdf_mf, AtVector(xx, yy, 0), 550, 0x1, true,
-                            out_wi, out_lobe_index, &lobe_sample);
-                        if (lobe_mask != AI_BSDF_LOBE_MASK_NONE) {
-                            result += lobe_sample.weight.r;
-                            ns++;
-                        }
-                    }
-                }
-                result /= float(samples * samples);
-                result = 1.0f - result;
-                lut[idx] = result;
-            }
-        }
-
-        auto lut_out = a2::LUT2D<float>(
-            lut, a2::Dimension{lut_resolution_i, 0.0f, 1.0f, true},
-            a2::Dimension{lut_resolution_j, 0.0f, 1.0f, false});
-            */
         auto fun_ggx_roughness_mu = [&](int i, int j, int lut_resolution_i,
                                         int lut_resolution_j) {
 
@@ -115,9 +65,21 @@ shader_evaluate {
         auto lut_out = a2::LUT2D<float>(fun_ggx_roughness_mu,
                                         a2::Dimension{32, 0.0f, 1.0f, true},
                                         a2::Dimension{32, 0.0f, 1.0f, false});
+        write(lut_out, "ggx_E");
 
-        lut_out.write("ggx_roughness_mu");
+        auto fun_E_avg = [&](int i, int n) {
+            float result = 0.0f;
+            for (int j = 0; j < n; ++j) {
+                result += 1.0f -
+                          lut_out.lookup(float(i) / float(n - 1),
+                                         float(j) / float(n));
+            }
+            return result / float(n);
+        };
 
+        auto lut_E_avg =
+            a2::LUT1D<float>(fun_E_avg, a2::Dimension{32, 0.0f, 1.0f, true});
+        write(lut_E_avg, "ggx_E_avg");
         display = AI_RGB_GREEN;
     }
 
--- a2_uber/lut.hpp
+++ a2_uber/lut.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
 #include "common/util.hpp"
-#include <OpenImageIO/imagebuf.h>
-#include <fstream>
-#include <memory>
-#include <spdlog/fmt/ostr.h>
+#include <functional>
 #include <vector>
 
 namespace a2 {
@@ -20,12 +17,44 @@ struct Dimension {
     bool inclusive;
 };
 
+template <typename T> class LUT1D {
+public:
+    std::vector<T> _data;
+    Dimension _dim_x;
+
+    LUT1D(const std::vector<T>& data, const Dimension& dim_x)
+        : _data(data), _dim_x(dim_x) {}
+
+    LUT1D(std::function<T(int, int)> fun, const Dimension& dim_x)
+        : _dim_x(dim_x) {
+        _data.resize(_dim_x.resolution);
+        for (int i = 0; i < _dim_x.resolution; ++i) {
+            _data[i] = fun(i, _dim_x.resolution);
+        }
+    }
+
+    auto lookup(float x) -> T {
+        float xx = clamp((x - _dim_x.start) * _dim_x.inv_range, 0.0f, 1.0f);
+        int inc_x = _dim_x.inclusive ? 1 : 0;
+        xx *= (_dim_x.resolution - inc_x);
+        float fx = floorf(xx);
+        float dx = xx - fx;
+        int ix0 = int(fx);
+        int ix1 = ix0 + 1;
+        ix0 = clamp(ix0, 0, _dim_x.resolution - 1);
+        ix1 = clamp(ix1, 0, _dim_x.resolution - 1);
+        float a = _data[ix0];
+        float b = _data[ix1];
+        return lerp(a, b, dx);
+    }
+};
+
 template <typename T> class LUT2D {
+public:
     std::vector<T> _data;
     Dimension _dim_x;
     Dimension _dim_y;
 
-public:
     LUT2D(const std::vector<T>& data, const Dimension& dim_x,
           const Dimension& dim_y)
         : _data(data), _dim_x(dim_x), _dim_y(dim_y) {}
@@ -43,10 +72,8 @@ public:
     }
 
     auto lookup(float x, float y) -> T {
-        float xx =
-            clamp((x - _dim_x.start) / (_dim_x.end - _dim_x.start), 0.0f, 1.0f);
-        float yy =
-            clamp((y - _dim_y.start) / (_dim_y.end - _dim_y.start), 0.0f, 1.0f);
+        float xx = clamp((x - _dim_x.start) * _dim_x.inv_range, 0.0f, 1.0f);
+        float yy = clamp((y - _dim_y.start) * _dim_y.inv_range, 0.0f, 1.0f);
         int inc_x = _dim_x.inclusive ? 1 : 0;
         int inc_y = _dim_y.inclusive ? 1 : 0;
         xx *= (_dim_x.resolution - inc_x);
@@ -70,49 +97,6 @@ public:
         float A = lerp(a, b, dx);
         float B = lerp(c, d, dx);
         return lerp(A, B, dy);
-    }
-
-    void write(const std::string& name) {
-        OIIO::ImageSpec spec(_dim_x.resolution, _dim_y.resolution, 1,
-                             OIIO::TypeDesc::FLOAT);
-        auto output_img = std::make_unique<OIIO::ImageBuf>(
-            fmt::format("lut_{}.exr", name), spec);
-        output_img->set_write_format(OIIO::TypeDesc::HALF);
-
-        std::fstream fs_src;
-        fs_src.open(fmt::format("lut_{}.hpp", name), std::fstream::out);
-
-        fmt::print(fs_src, "#pragma once\n"
-                           "#include \"a2_uber/lut.hpp\"\n"
-                           "inline auto get_lut_{}() -> "
-                           "std::unique_ptr<a2::LUT2D<float>> {{\n",
-                   name);
-
-        fmt::print(
-            fs_src,
-            "return std::make_unique<a2::LUT2D<float>>(std::vector<float>{{\n",
-            _dim_x.resolution * _dim_y.resolution);
-
-        for (int j = 0; j < _dim_y.resolution; ++j) {
-            for (int i = 0; i < _dim_x.resolution; ++i) {
-                auto idx = j * _dim_x.resolution + i;
-                fmt::print(fs_src, "{}", _data[idx]);
-                output_img->setpixel(i, j, &_data[idx]);
-                if (idx != _dim_x.resolution * _dim_y.resolution - 1)
-                    fmt::print(fs_src, ", ");
-            }
-            fmt::print(fs_src, "\n");
-        }
-        fmt::print(fs_src, "}},\n");
-        fmt::print(fs_src, "a2::Dimension({}, {}, {}, {}), ", _dim_x.resolution,
-                   _dim_x.start, _dim_x.end, _dim_x.inclusive);
-        fmt::print(fs_src, "a2::Dimension({}, {}, {}, {}));\n",
-                   _dim_y.resolution, _dim_y.start, _dim_y.end,
-                   _dim_y.inclusive);
-
-        fmt::print(fs_src, "}}\n");
-
-        output_img->write(fmt::format("lut_{}.exr", name));
     }
 };
 }
