15def345c774ee4dac60d1be3f5ceb22bde114f9
Anders Langlands <anderslanglands@gmail.com>
2017-08-21 21:29:31 +1200

    split lut outptu functions to avoid dependencies on oiio

---
+++ a2_uber/lut_output.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "a2_uber/lut.hpp"
+#include <OpenImageIO/imagebuf.h>
+#include <fstream>
+#include <memory>
+#include <spdlog/fmt/ostr.h>
+
+namespace a2 {
+
+template <typename T>
+inline void write(const LUT1D<T>& lut, const std::string& name) {
+    OIIO::ImageSpec spec(lut._dim_x.resolution, 1, 1, OIIO::TypeDesc::FLOAT);
+    auto output_img =
+        std::make_unique<OIIO::ImageBuf>(fmt::format("lut_{}.exr", name), spec);
+    output_img->set_write_format(OIIO::TypeDesc::HALF);
+
+    std::fstream fs_src;
+    fs_src.open(fmt::format("lut_{}.hpp", name), std::fstream::out);
+
+    fmt::print(fs_src, "#pragma once\n"
+                       "#include \"a2_uber/lut.hpp\"\n"
+                       "inline auto get_lut_{}() -> "
+                       "std::unique_ptr<a2::LUT1D<float>> {{\n",
+               name);
+
+    fmt::print(
+        fs_src,
+        "return std::make_unique<a2::LUT1D<float>>(std::vector<float>{{\n",
+        lut._dim_x.resolution);
+
+    for (int i = 0; i < lut._dim_x.resolution; ++i) {
+        fmt::print(fs_src, "{}", lut._data[i]);
+        output_img->setpixel(i, 0, &lut._data[i]);
+        if (i != lut._dim_x.resolution - 1)
+            fmt::print(fs_src, ", ");
+    }
+    fmt::print(fs_src, "\n");
+    fmt::print(fs_src, "}},\n");
+    fmt::print(fs_src, "a2::Dimension({}, {}, {}, {})", lut._dim_x.resolution,
+               lut._dim_x.start, lut._dim_x.end, lut._dim_x.inclusive);
+
+    fmt::print(fs_src, "}}\n");
+
+    output_img->write(fmt::format("lut_{}.exr", name));
+}
+
+template <typename T>
+inline void write(const LUT2D<T>& lut, const std::string& name) {
+    OIIO::ImageSpec spec(lut._dim_x.resolution, lut._dim_y.resolution, 1,
+                         OIIO::TypeDesc::FLOAT);
+    auto output_img =
+        std::make_unique<OIIO::ImageBuf>(fmt::format("lut_{}.exr", name), spec);
+    output_img->set_write_format(OIIO::TypeDesc::HALF);
+
+    std::fstream fs_src;
+    fs_src.open(fmt::format("lut_{}.hpp", name), std::fstream::out);
+
+    fmt::print(fs_src, "#pragma once\n"
+                       "#include \"a2_uber/lut.hpp\"\n"
+                       "inline auto get_lut_{}() -> "
+                       "std::unique_ptr<a2::LUT2D<float>> {{\n",
+               name);
+
+    fmt::print(
+        fs_src,
+        "return std::make_unique<a2::LUT2D<float>>(std::vector<float>{{\n",
+        lut._dim_x.resolution * lut._dim_y.resolution);
+
+    for (int j = 0; j < lut._dim_y.resolution; ++j) {
+        for (int i = 0; i < lut._dim_x.resolution; ++i) {
+            auto idx = j * lut._dim_x.resolution + i;
+            fmt::print(fs_src, "{}", lut._data[idx]);
+            output_img->setpixel(i, j, &lut._data[idx]);
+            if (idx != lut._dim_x.resolution * lut._dim_y.resolution - 1)
+                fmt::print(fs_src, ", ");
+        }
+        fmt::print(fs_src, "\n");
+    }
+    fmt::print(fs_src, "}},\n");
+    fmt::print(fs_src, "a2::Dimension({}, {}, {}, {}), ", lut._dim_x.resolution,
+               lut._dim_x.start, lut._dim_x.end, lut._dim_x.inclusive);
+    fmt::print(fs_src, "a2::Dimension({}, {}, {}, {}));\n",
+               lut._dim_y.resolution, lut._dim_y.start, lut._dim_y.end,
+               lut._dim_y.inclusive);
+
+    fmt::print(fs_src, "}}\n");
+
+    output_img->write(fmt::format("lut_{}.exr", name));
+}
+}
